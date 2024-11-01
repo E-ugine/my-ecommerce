31e89748b70f816324eaff5d77342d3f1f536a56
EugineAgolla <agollaeugine@gmail.com>
2024-11-02 02:21:56 +0300

    bg color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,17 @@ const handleCount = () => {
     <>
       <button onClick={handleCount} ></button>
       <h1 className="text-3xl font-bold" >Count is {count} </h1>
+      <div className="h-full border-2 border-gray-200 
+                border-opacity-60 rounded-lg 
+                overflow-hidden">
+        <div className="p-6 hover:bg-green-600 
+                    hover:text-white transition 
+                    duration-300 ease-in">
+            <h1 className="text-2xl font-semibold mb-3">
+                Hover
+            </h1>
+        </div>
+    </div>
     </>
   )
 }
